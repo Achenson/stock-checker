f84c4b460ddcf5f145ee27884ffb64897267ad13
Achenson <ach.01.mail@gmail.com>
2019-09-08 14:31:24 +0200

    test for mocha initialised

---
+++ test/func_tests.js
@@ -0,0 +1,52 @@
+/*
+*
+*
+*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]-----
+*       (if additional are added, keep them at the very end!)
+*/
+
+var chaiHttp = require('chai-http');
+var chai = require('chai');
+var assert = chai.assert;
+var server = require('../server');
+
+chai.use(chaiHttp);
+
+
+    
+    describe('GET /api/stock-prices => stockData object', function() {
+      
+      it('1 stock', function(done) {
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog'})
+        .end(function(err, res){
+          
+          //complete this one too
+          
+          done();
+        });
+      });
+      
+      it('1 stock with like', function(done) {
+
+        
+        
+      });
+      
+      it('1 stock with like again (ensure likes arent double counted)', function(done) {
+        
+      });
+      
+      it('2 stocks', function(done) {
+        
+      });
+      
+      it('2 stocks with like', function(done) {
+        
+      });
+      
+    });
+
+
