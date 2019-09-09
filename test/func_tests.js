3f330a51edf2e9aea0a09f3762dda92dba47417b
Achenson <ach.01.mail@gmail.com>
2019-09-09 20:12:22 +0200

    all tests passing

--- test/func_tests.js
+++ test/func_tests.js
@@ -1,87 +1,113 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
+var chaiHttp = require("chai-http");
+var chai = require("chai");
 var assert = chai.assert;
-var server = require('../server');
+var server = require("../server");
 
 chai.use(chaiHttp);
 
+describe("GET /api/stock-prices => stockData object", function() {
+  it("1 stock", function(done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "goog" })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body[0].stock, "GOOG");
+        assert.property(res.body[0], "price");
+        assert.property(res.body[0], "likes");
 
-    
-    describe('GET /api/stock-prices => stockData object', function() {
-      
-      it('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-            assert.equal(res.status, 200);
-            assert.equal(res.type, "application/json");
-            assert.equal(res.body[0].stock, "GOOG");
+        //complete this one too
 
-          
-          //complete this one too
-          
-          done();
-        });
+        done();
       });
-      
-      it('1 stock with like', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-            assert.equal(res.status, 200);
-            assert.equal(res.type, "application/json");
-            assert.equal(res.body[0].stock, "GOOG");
+  });
 
+  it("1 stock with like", function(done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "goog" })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body[0].stock, "GOOG");
+        assert.property(res.body[0], "price");
+        assert.property(res.body[0], "likes");
 
-          
-          //complete this one too
-          
-          done();
-        });
+        //complete this one too
 
-
-
-        
+        done();
       });
-      
-      it('1 stock with like again (ensure likes arent double counted)', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-            assert.equal(res.status, 200);
-            assert.equal(res.type, "application/json");
-            assert.equal(res.body[0].stock, "GOOG");
-            assert.equal(res.body[0].likes, 1)
+  });
 
+  it("1 stock with like again (ensure likes arent double counted)", function(done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "goog" })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body[0].stock, "GOOG");
+        assert.equal(res.body[0].likes, 1);
+        assert.property(res.body[0], "price");
+        assert.property(res.body[0], "likes");
 
-          
-          //complete this one too
-          
-          done();
-        });
+        //complete this one too
 
-        
+        done();
       });
-      
-      it('2 stocks', function(done) {
-        
-      });
-      
-      it('2 stocks with like', function(done) {
-        
-      });
-      
-    });
+  });
 
+  it("2 stocks", function(done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["goog", "msft"] })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body[0].stock, "GOOG");
+        assert.property(res.body[0], "price");
+        assert.property(res.body[0], "rel_likes");
+        assert.equal(res.body[1].stock, "MSFT");
+        assert.property(res.body[1], "price");
+        assert.property(res.body[1], "rel_likes");
 
+        //complete this one too
+
+        done();
+      });
+  });
+
+  it("2 stocks with like", function(done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["goog", "msft"] })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.equal(res.body[0].stock, "GOOG");
+        assert.property(res.body[0], "price");
+        assert.property(res.body[0], "rel_likes");
+        assert.equal(res.body[1].stock, "MSFT");
+        assert.property(res.body[1], "price");
+        assert.property(res.body[1], "rel_likes");
+
+        //complete this one too
+
+        done();
+      });
+  });
+});
